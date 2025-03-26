1fc56735d249bf9d607765970c1e3fe5f6d29f58
Dineth <dinethfiverr23@gmail.com>
2025-03-26 10:44:43 +0530

    Adding Code To Views And Create Result.html Page And Extract Data Using Requests And BS4

--- Scraper/views.py
+++ Scraper/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+
 
 # Create your views here.
+
+def Scrape(request):
+
+    url = 'https://www.google.com'
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    link_address = []
+
+    for link in soup.find_all('a'):
+        link_address.append(link.get('href'))
+
+    context = {
+        'link_address': link_address
+    }
+
+
+    return render(request, 'scraper/result.html', context)
